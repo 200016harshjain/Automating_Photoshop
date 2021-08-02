28ce443d0a1089ee2fed8f321a407416444302ba
200016harshjain <44161491+200016harshjain@users.noreply.github.com>
2021-08-02 17:52:25 +0530

    Update photoshop.py

--- codes/photoshop.py
+++ codes/photoshop.py
@@ -12,12 +12,11 @@ doc = psApp.Application.ActiveDocument
 
 filename = r"C:\Users\ranja\Desktop\Scripts\student_data.csv"
 df = pd.read_csv(filename)
-print(df.head())
-
-data_dict = df.to_dict('index')
+data_dict = df.to_dict('index')  
 
 
-#Manipulating the exact layers
+
+#Picking the layers from the Photoshop document which we need to feed data into from a data source
 layer_facts = doc.ArtLayers["NAME"]
 text_of_layer = layer_facts.TextItem
 text_of_layer.contents = "This is an example of a new text."
@@ -31,26 +30,28 @@ smp_name_text = SMP_Name.TextItem
 smp_name_text.contents  = "random smp"
 
 
-#Properties of like files we'll be making
+#Properties of the image files we'll be generating
 options = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb')
 options.Format = 13   # PNG
 options.PNG8 = False  # Sets it to PNG-24 bit
 
+#location of the folder where we'd be saving the outputs
 exportRoot = "C:/Users/ranja/Desktop/Scripts/"
 
-# For every key and value in the items of the dictionary "dict_sample"
-#loop the data and print
+
+#loop through the  data 
 for dict_number,row in data_dict.items():
     
-    # Replace Text of text Layer
-    
+    # Replace the value of each photshop layer with data obtained from the data source
     text_of_layer.contents = row['Name']
     smp_name_text.contents  = row['SMP']
     sig_name_text.contents = row['SIG']
-
+    
+    # save the created files in specific folders by checking to see if they exist or not
     folder =  exportRoot + row['SIG'] + "/" +  row['SMP'] + "/" 
     if not os.path.exists(folder):
         os.makedirs(folder)
     fileName =  folder + row['Name'] + ".png"
     
+
     doc.Export(ExportIn=fileName, ExportAs=2, Options=options)
